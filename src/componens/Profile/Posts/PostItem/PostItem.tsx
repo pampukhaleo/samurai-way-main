dca18eaef1f7cec9e4f7c1ed0ac4edc545ec5579
pampukhaleo <lgpampukha@gmail.com>
2023-07-21 15:52:14 +0300

    post item comp

---
+++ src/componens/Profile/Posts/PostItem/PostItem.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './postItem.css'
+
+type PostItemType = {
+  profilePicture: string
+  text: string
+}
+
+export const PostItem = ({profilePicture, text}: PostItemType) => {
+  return (
+    <li className="post">
+      <img src={profilePicture} className="post_circle" alt='profile-picture'/>
+      <span className="post-text">{text}</span>
+    </li>
+  )
+}
